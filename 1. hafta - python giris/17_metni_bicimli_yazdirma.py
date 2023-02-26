371f0cab8718319a7ff77e7696f632a8f0f44920
Ömer Faruk BARAN <of123baran@outlook.com>
2023-02-26 16:36:27 +0300

    f string

--- 1. hafta - python giris/17_metni_bicimli_yazdirma.py
+++ 1. hafta - python giris/17_metni_bicimli_yazdirma.py
@@ -4,3 +4,4 @@ ayva = 2
 istek = "Ben {} elma {} armut {} ayva istiyorum."
 print(istek.format(elma, armut, ayva))
 print("Ben {} elma {} armut {} ayva istiyorum.".format(elma, armut, ayva))
+print(f"Ben {elma} elma {armut} armut {ayva} ayva istiyorum.") # Python 3.6+ daha kolay bir yazım şekli
